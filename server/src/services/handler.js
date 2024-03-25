3c32b0f0c7d97dd7e5dbf095be6aef327b7010ab
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2024-03-25 23:56:12 +0530

    "Added Service for Handling Edge cases using Handler"

---
+++ server/src/services/handler.js
@@ -0,0 +1,13 @@
+const handler = (fn) => async (req, res,next) => {
+    try {
+        await fn(req, res,next);
+
+    } catch (error) {
+        res.status(error.code).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+export default handler;
