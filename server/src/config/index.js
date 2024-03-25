95fce6207cc3ef0f456441ea5a411177b535e91f
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2024-03-25 22:53:27 +0530

    "Added JWT "

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -3,7 +3,9 @@ dotenv.config();
 
 const config = {
     PORT: process.env.PORT || 4000,
-    DATABASE_URI: process.env.DATABASE_URI
+    DATABASE_URI: process.env.DATABASE_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
+    JWT_EXPIRE: process.env.JWT_EXPIRE,
 }
 
 export default config;
