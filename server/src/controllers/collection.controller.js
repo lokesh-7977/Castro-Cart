cfdc21a1457e55ede8151e89db3be7669c209c2e
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2024-03-26 23:28:53 +0530

    "Add collection controller with CRUD operations"

---
+++ server/src/controllers/collection.controller.js
@@ -0,0 +1,71 @@
+import Collection from "../models/collection.schema";
+import handler from "../services/handler";
+import CustomError from "../services/customError";
+
+
+export const createCollection = handler(async (req, res) => {
+    const {name} = req.body;
+
+    if (!name) {
+        throw new CustomError("Please provide name", 400);
+    }
+
+   const collection = await  Collection.create({ name });
+   res.status(200).json({
+       success : true,
+       message : "Collection created successfully in DB",
+       collection
+   })
+});
+
+
+export const updateCollection = handler(async (req, res) => {
+    const {name} = req.body;
+    const  { id : collectionId } = req.params;
+
+    if (!name) {
+        throw new CustomError("Please provide name", 400);
+    }
+
+    const updatedCollection = await  Collection.findByIdAndUpdate(collectionId, { name }, { new : true , runValidators : true });
+
+    if(!updatedCollection) {
+        throw new CustomError("Collection not found", 404);
+    }
+   res.status(200).json({
+       success : true,
+       message : "Collection updated successfully in DB",
+       collection
+   })
+});
+
+export const deleteCollection = handler(async (req, res) => {
+    const { id : collectionId } = req.params;
+    const deleteCollection = await Collection.findByIdAndDelete(collectionId);
+
+    if(!deleteCollection) {
+        throw new CustomError("Collection to delete  not found", 404);
+    }
+    res.status(200).json({
+        success : true,
+        message : "Collection deleted successfully in DB",
+        collection
+    })
+});
+
+
+
+export const getAllCollections = handler(async (req, res) => {
+    const collections = await Collection.find();
+
+    if(!collections) {
+        throw new CustomError("No collections found", 404);
+    }
+
+    res.status(200).json({
+        success : true,
+        message : "All collections fetched successfully",
+        collections
+    })
+
+});
