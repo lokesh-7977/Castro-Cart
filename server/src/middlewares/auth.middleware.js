4f55df33f3794e3d738331bbe77ab668cef57ac7
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2024-03-26 23:08:36 +0530

    "Add auth middleware for user authentication and authorization"

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,38 @@
+import User from '../models/user.schema';
+import JWT from 'jsonwebtoken';
+import handler from '../services/handler';
+import config from '../config/index';
+import CustomError from '../services/customError';
+
+
+export const isLoggedIn = (handler(async (req, res, next) => {
+    let token;
+
+    if(req.cookies.token || (req.headers.authorization && req.headers.authorization.startsWith('Bearer'))){
+        token = req.cookies.token || req.headers.authorization.split(' ')[1];
+    }
+
+    if(!token){
+        throw new CustomError('No Access to this resource', 401);
+    }
+
+    try {
+       const decodedPayload =  JWT.verify(token, config.JWT_SECRET);
+       req.user = await User.findById(decodedPayload._id,"name email role");
+       next();
+
+    } catch (error) {
+        throw new CustomError('No Access to this resource', 401);
+    }
+    next();
+
+
+}));
+
+
+
+export const authorize = (...requiredRoles)=> handler(async (req,res,next) => {
+    if(!requiredRoles.includes(req.user.role)){
+        throw new CustomError('You are not authorized to access', 403);
+    }
+});
