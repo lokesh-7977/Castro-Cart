70b6da2a4331f0db70893cbe5ed8ecc35e4d9eb9
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2024-03-27 17:52:43 +0530

    "Add auth routes and controller functions"

---
+++ server/src/routes/auth.route.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import { signup,login, logout , getProfile } from '../controllers/auth.controller.js';
+import { isLoggedIn } from '../middlewares/auth.middleware.js';
+
+const router = Router();
+
+router.post('/signup',signup)
+router.post('/login',login)
+router.get('/logout',logout)
+
+
+router.get('/profile',isLoggedIn,getProfile)
+
+
+
+export default Router;
