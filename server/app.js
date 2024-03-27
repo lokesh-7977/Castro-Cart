cea7610f7a3c1e292ed1556392f0d40eba1b2301
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2024-03-27 17:53:36 +0530

    "Updated app.js with new routes and error handling"

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import routes from "./src/routes/index.js"
 
 const app = express();
 
@@ -8,6 +10,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
+
+
+
+app.get("/", (_req, res) => {
+  res.send("Welcome to  Ecomerce API Created By Lokesh Medharametla");
+});
+
+app.use("/api", routes);
+
+
+
+
+
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "Route Not Found" });
+});
 
 
 export default app;
