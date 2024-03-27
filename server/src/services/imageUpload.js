c26322c1814a1e9ba5862a06837a43a78d683d64
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2024-03-27 11:48:40 +0530

    "Add imageUpload service functions"

---
+++ server/src/services/imageUpload.js
@@ -0,0 +1,21 @@
+import config from "../config/index";
+
+
+export const s3FileUpload = async ({ bucketName,key,body,contentType }) => {
+    return await s3.upload({
+        Bucket : bucketName,
+        Key : key,
+        Body : body,
+        ContentType : contentType
+
+    })
+    .promise()
+}
+
+export const s3FileDelete = async ({ bucketName,key,body,contentType }) => {
+    return await s3.deleteObject({
+        Bucket : bucketName,
+        Key : key,
+    })
+    .promise()
+}
