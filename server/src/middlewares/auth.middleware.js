d33e26d7455c47296b600494781bbe2271576fb4
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2024-03-28 17:53:21 +0530

    "Added Logic To middlleware"

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -4,35 +4,30 @@ import handler from '../services/handler.js';
 import config from '../config/index.js';
 import CustomError from '../services/customError.js';
 
-
-export const isLoggedIn = (handler(async (req, res, next) => {
+export const isLoggedIn = handler(async (req, res, next) => {
     let token;
 
-    if(req.cookies.token || (req.headers.authorization && req.headers.authorization.startsWith('Bearer'))){
+    if (req.cookies.token || (req.headers.authorization && req.headers.authorization.startsWith('Bearer'))) {
         token = req.cookies.token || req.headers.authorization.split(' ')[1];
     }
 
-    if(!token){
+    if (!token) {
         throw new CustomError('No Access to this resource', 401);
     }
 
     try {
-       const decodedPayload =  JWT.verify(token, config.JWT_SECRET);
-       req.user = await User.findById(decodedPayload._id,"name email role");
-       next();
-
+        const decodedPayload = JWT.verify(token, config.JWT_SECRET);
+        req.user = await User.findById(decodedPayload._id, "name email role");
+        next();
     } catch (error) {
         throw new CustomError('No Access to this resource', 401);
     }
-    next();
-
-
-}));
+});
 
 
 
-export const authorize = (...requiredRoles)=> handler(async (req,res,next) => {
-    if(!requiredRoles.includes(req.user.role)){
-        throw new CustomError('You are not authorized to access', 403);
+export const authorize = (...requiredRoles) => handler(async (req, res, next) => {
+    if (!requiredRoles.includes(req.user.role)) {
+        throw new CustomError('You are not authorized to access this resource', 403);
     }
 });
