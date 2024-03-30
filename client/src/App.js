b65a4f1fa92301001e1f249357f032b9980d6627
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2024-03-30 19:53:59 +0530

    "Initilized Store from redux-toolkit"

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
+import Navbar from './Components/Navbar/Navbar'
+import Landing from './Components/Landing/Landing'
+import Brands from './Components/BrandBars/Brands'
+// import Deals from './Components/Deals/Deals'
 
 const App = () => {
   return (
     <>
-    
+    <Navbar />
+       <Landing />
+       <Brands />
+      {/* <Deals />  */}
     </>
    
   )
