b2f3b13ce0a01bec793148686fa26f3a6101ed1f
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2024-03-31 23:39:05 +0530

    "Refactor App.js and add Deals component"

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import React from 'react'
+import React from "react";
 
-import Navbar from './Components/Navbar/Navbar'
-import Landing from './Components/Landing/Landing'
-import Brands from './Components/BrandBars/Brands'
-import Footer from './Components/Footer/Footer'
-import Subscribers from './Components/Subsribers/Subscribers'
-// import Deals from './Components/Deals/Deals'
+import Navbar from "./Components/Navbar/Navbar";
+import Landing from "./Components/Landing/Landing";
+import Brands from "./Components/BrandBars/Brands";
+import Deals from "./Components/Deals/Deals";
+import Subscribers from "./Components/Subsribers/Subscribers";
+import Footer from "./Components/Footer/Footer";
 
 const App = () => {
   return (
     <>
-    <Navbar />
-       <Landing />
-       <Brands />
-      {/* <Deals />  */}
+      <Navbar />
+      <Landing />
+      <Brands />
+      <Deals />
       <Subscribers />
-      <Footer/>
+      <Footer />
     </>
-   
-  )
-}
+  );
+};
 
-export default App
+export default App;
