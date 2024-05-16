369617167b8bf20e5e5da7e86b245329c0f6b9ef
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2024-05-16 17:59:18 +0530

    added New Collections

---
+++ client/src/components/NewCollections/NewCollections.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from './NewCollections.module.css';
+
+const NewCollections = () => {
+  return (
+    <>
+            <div className={styles.new_collections}>
+                <h1>NEW COLLECTIONS</h1>
+                <hr />
+                <div className={styles.collections}>
+        {/* {collections.map((collection) => () => {})} */}
+                </div>
+               
+                </div>
+                
+
+    </>        
+)
+}
+
+export default NewCollections
