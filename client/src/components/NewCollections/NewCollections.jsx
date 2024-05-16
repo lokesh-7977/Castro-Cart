8a065d0b23948d3d1efc6c40a2f4c791e1c3691a
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2024-05-16 17:59:38 +0530

    Refactor NewCollections component and fix formatting

--- client/src/components/NewCollections/NewCollections.jsx
+++ client/src/components/NewCollections/NewCollections.jsx
@@ -1,21 +1,18 @@
-import React from 'react'
-import styles from './NewCollections.module.css';
+import React from "react";
+import styles from "./NewCollections.module.css";
 
 const NewCollections = () => {
   return (
     <>
-            <div className={styles.new_collections}>
-                <h1>NEW COLLECTIONS</h1>
-                <hr />
-                <div className={styles.collections}>
-        {/* {collections.map((collection) => () => {})} */}
-                </div>
-               
-                </div>
-                
+      <div className={styles.new_collections}>
+        <h1>NEW COLLECTIONS</h1>
+        <hr />
+        <div className={styles.collections}>
+          {/* {collections.map((collection) => () => {})} */}
+        </div>
+      </div>
+    </>
+  );
+};
 
-    </>        
-)
-}
-
-export default NewCollections
+export default NewCollections;
