8dc0a12fc43909b4148aba01e8bb3578554e5342
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2024-05-16 17:28:27 +0530

    feat: Add Popular component and its styles

---
+++ client/src/components/Popular/Popular.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './Popular.module.css'
+
+const Popular = () => {
+  return (
+    <>
+        <div className={styles.popular}>
+             <h1>POPULAR IN WOMEN</h1>
+             <hr />
+             <div className={styles.popular_item}>
+                {
+                    
+                }
+             </div>
+        </div>
+    </>
+  )
+}
+
+export default Popular
